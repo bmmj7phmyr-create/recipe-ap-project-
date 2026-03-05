7535639067890b03142129d0731b543b994aed5f
tun <username@users.noreply.github.com>
2026-03-06 02:59:44 +0300

    Docker setup completed

---
+++ app/calc.py
@@ -0,0 +1,12 @@
+'''
+Calculator function
+'''
+
+def add(x, y):
+          '''add x and y and return result.'''
+          return x + y
+def subtract(x, y):
+          '''subcract y from x and return result.'''
+          return y-x
+def topla(a,b):
+          return a+b          
