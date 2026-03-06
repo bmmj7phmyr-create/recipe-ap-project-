3d2ff6ac033ac92dce5dac14a3ab2b776f1651eb
tun <username@users.noreply.github.com>
2026-03-06 03:04:54 +0300

    Fix flake8 issues

--- app/tests.py
+++ app/tests.py
@@ -1,23 +1,22 @@
 from django.test import SimpleTestCase
-from app import calc 
+from app import calc
+
 
 class CalcTests(SimpleTestCase):
-    '''Test the calc module.'''
-          
+    """Test the calc module."""
+
     def test_add_numbers(self):
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.subtract(10,15)
-        self.assertEqual(res,5) 
-     
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
+
     def test_topla(self):
-        sonuc = calc.add(5,6)
-        self.assertEqual(sonuc,11)
+        sonuc = calc.add(5, 6)
+        self.assertEqual(sonuc, 11)
     
-    def test_topla(self):
-        sonuc=calc.topla(9,15)
-        self.assertEqual(sonuc,24)    
+       
               
                                   
