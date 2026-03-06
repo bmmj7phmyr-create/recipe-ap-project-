0166169de9ea359768360b437b3215e95165b93e
tun <username@users.noreply.github.com>
2026-03-06 22:54:00 +0300

    Setup custom user model and fix migration issue

---
+++ core/tests/test_model.py
@@ -0,0 +1,22 @@
+"""
+modeller için test
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """test modeli"""
+
+    def test_create_user_with_email_successful(self):
+        """email ve kullanıcı adıyla kullanıcı oluşturma başarılı"""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
