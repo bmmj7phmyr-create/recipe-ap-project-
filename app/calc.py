172edc8ca94a9c3cbde8370005fd75597ed2829b
tun <username@users.noreply.github.com>
2026-03-06 03:10:11 +0300

    calc.py

--- app/calc.py
+++ app/calc.py
@@ -1,12 +1,12 @@
-'''
-Calculator function
-'''
+'''Calculator function'''
 
 def add(x, y):
-          '''add x and y and return result.'''
-          return x + y
+    '''add x and y and return result.'''
+    return x + y
+
 def subtract(x, y):
-          '''subcract y from x and return result.'''
-          return y-x
-def topla(a,b):
-          return a+b          
+    '''subtract y from x and return result.'''
+    return x - y
+
+def topla(a, b):
+    return a + b
