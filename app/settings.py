ab3b7adb588e31f98aa298c2764cabbcc9c2d508
tun <username@users.noreply.github.com>
2026-03-07 15:23:16 +0300

    add API documentation with drf-spectacular

--- app/settings.py
+++ app/settings.py
@@ -129,6 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
 }
